f38e73d5fff3c0ea2a53a9e672f4339264f58d59
Mark Trevor Birss <markbirss@gmail.com>
2025-02-24 09:55:54 +0200

    Update GxEPD2_290_GDEY029T94.h

--- src/epd/GxEPD2_290_GDEY029T94.h
+++ src/epd/GxEPD2_290_GDEY029T94.h
@@ -33,7 +33,7 @@ class GxEPD2_290_GDEY029T94 : public GxEPD2_EPD
     static const uint16_t full_refresh_time = 1750; // ms, e.g. 1715001us
     static const uint16_t partial_refresh_time = 400; // ms, e.g. 362000us
     // constructor
-    GxEPD2_290_GDEY029T94(int16_t cs, int16_t dc, int16_t rst, int16_t busy);
+    GxEPD2_290_GDEY029T94(int16_t cs, int16_t dc, int16_t rst, int16_t busy, SPIClass &spi);
     // methods (virtual)
     //  Support for Bitmaps (Sprites) to Controller Buffer and to Screen
     void clearScreen(uint8_t value = 0xFF); // init controller memory and screen (default white)
