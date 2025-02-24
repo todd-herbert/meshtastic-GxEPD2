5c44883e1471f6f82cdbb74f6b1d56d3dc43bad2
Mark Trevor Birss <markbirss@gmail.com>
2025-02-24 15:08:45 +0200

    Update GxEPD2_BW.h

--- src/GxEPD2_BW.h
+++ src/GxEPD2_BW.h
@@ -62,6 +62,7 @@
 #include "epd/GxEPD2_371.h"
 #include "epd/GxEPD2_420.h"
 #include "epd/GxEPD2_420_M01.h"
+#include "epd/GxEPD2_420_GYE042A87.h"
 #include "epd/GxEPD2_579_GDEY0579T93.h"
 #include "epd/GxEPD2_583.h"
 #include "epd/GxEPD2_583_T8.h"
